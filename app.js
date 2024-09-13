c71e7499bbae6b11780d219fe6d1f9c066ab0e84
Hamza Riaz <hriaz9803@gmail.com>
2024-09-13 06:11:18 -0700

    Change Port Number

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const port = process.env.PORT || 8080;
+const port = 8080;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
